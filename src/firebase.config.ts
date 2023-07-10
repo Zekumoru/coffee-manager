0057a077154b6670a22ee7e7dca7ee1971be3f07
Zekumoru Dragonhart <dev@zekumoru.com>
2023-07-10 20:43:12 +0200

    Install and init firebase

---
+++ src/firebase.config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDC3_D3QL9alvrmAcRwP-oGuGszZC_8GWw',
+  authDomain: 'zekumoru-coffee-manager.firebaseapp.com',
+  projectId: 'zekumoru-coffee-manager',
+  storageBucket: 'zekumoru-coffee-manager.appspot.com',
+  messagingSenderId: '974845176450',
+  appId: '1:974845176450:web:78fd8d1a3001c82472e29b',
+};
+
+export default firebaseConfig;
